7dec57029b64b8b1ee924948fc79b18d0ca0ff34
Hazem Ragab <63895193+rhazem13@users.noreply.github.com>
2023-01-07 02:18:36 +0200

    Time: 34 ms (56.61%), Space: 24.1 MB (32.38%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public uint reverseBits(uint n) {
+        uint result = 0;
+        for (int i = 0; i < 32; result = result << 1 | (n & 1), n >>= 1, ++i);
+        return result;
+    }
+}
