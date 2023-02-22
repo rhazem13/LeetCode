f726924be21c5039f44761db5eb5b7b57fde6ee0
Hazem Ragab <63895193+rhazem13@users.noreply.github.com>
2023-02-22 17:28:19 +0200

    Time: 100 ms (16.47%), Space: 37.8 MB (98.47%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cs
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cs
@@ -1,28 +1,31 @@
 public class Solution {
     public int FindKthPositive(int[] arr, int k) {
-        if (arr == null || arr.Length == 0)
-            return 0;
-        
-        int prev = 0,
-            count = 0;
-        
-        for (int i = 0; i < arr.Length; i++)
+    int count = 1;
+    int missingCount = 0;
+    int i = 0;
+    while (i < arr.Length)
+    {
+        if (arr[i] == count)
         {
-            if (count + arr[i] - prev - 1 >= k)
-            {
-                k -= count;
-                
-                while (k-- > 0)
-                    prev++;
-                
-                return prev;
-            }
-            else
-                count += (arr[i] - prev - 1);
-            
-            prev = arr[i];
+            i++;
+            count++;
         }
-        
-        return arr[arr.Length - 1] + k - count;
+        else
+        {
+            while (arr[i] != count)
+            {
+                if (k-1 == missingCount)
+                    return count;
+                missingCount++;
+                count++;
+            }
+        }
     }
+    while (missingCount < k - 1)
+    {
+        missingCount++;
+        count++;
+    }
+    return count;
+}
 }
