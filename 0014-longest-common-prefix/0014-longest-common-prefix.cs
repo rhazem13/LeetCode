ec79bb322900e2ff9cc9df465e8add591d72a653
Hazem Ragab <63895193+rhazem13@users.noreply.github.com>
2023-07-02 05:47:12 +0300

    Time: 105 ms (41.63%), Space: 39.9 MB (70.70%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -1,25 +1,23 @@
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-        int minLength=200;
-        for(int i=0;i<strs.Length;i++){
-            if(strs[i].Length<minLength)
-                minLength=strs[i].Length;
-        }
-        int builderIndex=0,ans=0;
-        while(builderIndex<minLength){
-            bool isPre=true;
-            char c=strs[0][builderIndex];
-            for(int i=1;i<strs.Length;i++){
-                if(strs[i][builderIndex]!=c)
-                    isPre=false;
+        // solve by for each letter we compare all of them yk then we return word based on counter
+        int count=0;
+        int i=0;
+        bool diff=false;
+        while(i<strs[0].Length){
+            for(int j=1;j<strs.Length;j++){
+                if(i>=strs[j].Length)
+                    diff=true;
+                else if(strs[j][i]!=strs[0][i])
+                    diff=true;
             }
-            if(isPre)
-                ans++;
-            else{
+            if(!diff)
+                count++;
+            
+            else
                 break;
-            }
-            builderIndex++;
+            i++;
         }
-        return strs[0].Substring(0,ans);
+        return strs[0].Substring(0,count);
     }
 }
