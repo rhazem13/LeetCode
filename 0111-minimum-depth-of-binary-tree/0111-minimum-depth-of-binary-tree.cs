506b9b9b0e95353b6f23af6f44bc571d9cae93a1
Hazem Ragab <63895193+rhazem13@users.noreply.github.com>
2023-07-10 21:14:56 +0300

    Time: 267 ms (50.65%), Space: 68.4 MB (30.39%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cs
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cs
@@ -1,27 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution {
-    public int dfs(TreeNode root, int d){
-        if(root==null)
-            return Int32.MaxValue;
-        if(root.left==null&&root.right==null)
-            return d;
-        return Math.Min(dfs(root.left,d+1),dfs(root.right,d+1));
+    public int mindepth;
+    public void helper(TreeNode root, int depth){
+        if(root!=null){
+            if(root.left==null && root.right==null){
+                if(depth<mindepth)
+                    mindepth=depth;
+            }
+            depth++;
+            helper(root.left,depth);
+            helper(root.right,depth);
+        }
     }
     public int MinDepth(TreeNode root) {
-        if(root==null)
-            return 0;
-        return dfs(root,1);
+        if(root==null) return 0;
+        mindepth = Int32.MaxValue;
+        helper(root,1);
+        return mindepth;
     }
 }
