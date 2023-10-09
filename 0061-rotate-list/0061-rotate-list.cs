2a53b015efbe19084fe8d2ace13b099518481ae1
Hazem Ragab <63895193+rhazem13@users.noreply.github.com>
2023-10-09 14:27:50 +0300

    Time: 474 ms (5.10%), Space: 39.5 MB (55.71%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cs
+++ 0061-rotate-list/0061-rotate-list.cs
@@ -1,38 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode RotateRight(ListNode head, int k) {
-        if (head == null || head.next == null)
-            return head;
-        
-        ListNode n1 = head,
-                 n2 = head,
-                 n3 = null;
-        int i = 1;
-        
-        while (n1.next != null)
-        {
-            n1 = n1.next;
-            i++;
+        if(k==0 || head==null) return head;
+        // get length
+        ListNode tmp = head;
+        int length=1;
+        while(tmp.next!=null){
+            tmp=tmp.next;
+            length++;
         }
-        
-        if (k > i)
-            k %= i;
-        
-        if (k == i || k == 0)
-            return head;
-        
-        i -= k;
-        
-        while (i > 0)
-        {
-            n3 = n2;
-            n2 = n2.next;
-            
-            i--;
+        if( k%length==0) return head;
+        // if k > lenth subtract using absolute value
+        while(k>=length)
+            k=Math.Abs(length-k);
+        ListNode lastprev=head,last=head;
+        for(int i=0;i<length-k;i++){
+            lastprev=last;
+            last=last.next;
         }
-        
-        n3.next = null;
-        n1.next = head;
-        
-        return n2;
+        tmp=last;
+        lastprev.next=null;
+        while(last.next!=null)
+            last=last.next;
+        last.next=head;
+        head=tmp;
+        return head;
     }
 }
