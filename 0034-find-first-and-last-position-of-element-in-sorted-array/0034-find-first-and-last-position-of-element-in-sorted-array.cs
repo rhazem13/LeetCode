984760e7007d2b8c3737b363d4922a6f7e141116
Hazem Ragab <63895193+rhazem13@users.noreply.github.com>
2023-10-09 16:24:11 +0300

    Time: 145 ms (14.46%), Space: 45.1 MB (45.00%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cs
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cs
@@ -2,39 +2,52 @@ public class Solution {
     public int[] SearchRange(int[] nums, int target) {
         if (nums == null || nums.Length == 0)
             return new int[] { -1, -1 };
-        
-        int i = 0, j = nums.Length - 1, k = 0, l = -1;
-        
-        while (i <= j)
-        {
-            k = i + (j - i) / 2;
-            
-            if (nums[k] == target)
-            {
-                l = k;
-                break;
+        if (nums.Length==1){
+            if(nums[0]==target) return new int[] {0,0};
+        }
+        // binary search until left is dif num or boundary
+        int l=0,m=-1,r=nums.Length-1;
+        int[] result = {-1,-1};
+        while(l<=r){
+            m=(l+r)/2;
+            if(nums[m]<target)
+                l=m+1;
+            if(nums[m]>target)
+                r=m-1;
+            if(nums[m]==target){
+                if(m==0){
+                    result[0]=m;
+                    break;
+                }
+                if(nums[m-1]!=target){
+                    result[0]=m;
+                    break;
+                }
+                r=m-1;
             }
-            else if (nums[k] < target)
-                i = k + 1;
-            else
-                j = k - 1;
         }
-        
-        if (l != -1)
-        {
-            i = l;
-            j = l;
-            
-            while (i - 1 >= 0 && nums[i - 1] == nums[i])
-                i--;
-                
-            while (j + 1 <= nums.Length - 1 && nums[j + 1] == nums[j])
-                j++;
-                
-            return new int[] { i, j };
+        m=-1;
+        l=0;
+        r=nums.Length-1;
+        // do same with right
+        while(l<=r){
+            m=(l+r)/2;
+            if(nums[m]<target)
+                l=m+1;
+            if(nums[m]>target)
+                r=m-1;
+            if(nums[m]==target){
+                if(m==nums.Length-1){
+                    result[1]=m;
+                    break;
+                }
+                if(nums[m+1]!=target){
+                    result[1]=m;
+                    break;
+                }
+                l=m+1;
+            }
         }
-        
-        
-        return new int[] { -1, -1 }; 
+        return result;
     }
 }
