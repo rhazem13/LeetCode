8864e5d1d9bf2b4dce4f06f766a556216e0c4640
Hazem <rhazem13@yahoo.com>
2024-08-08 13:56:32 +0300

    Time: 77 ms (23.55%), Space: 45.4 MB (28.57%) - LeetHub

---
+++ 3162-find-the-number-of-good-pairs-i/3162-find-the-number-of-good-pairs-i.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int NumberOfPairs(int[] nums1, int[] nums2, int k) {
+        int ans=0;
+        for(int i=0;i<nums1.Length;i++){
+            for(int j=0;j<nums2.Length;j++){
+                if(nums1[i]%(nums2[j]*k)==0)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+}
