728b757c87625050c97c6e014fc3298c956b884e
Hazem <rhazem13@yahoo.com>
2024-08-09 10:47:41 +0300

    Time: 23 ms (42.25%), Space: 26.8 MB (53.52%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int MinBitFlips(int start, int goal) {
+        int xor= start^goal;
+        int res=0;
+        while(xor > 0){
+            res+= xor&1;
+            xor>>=1;
+        }
+        return res;
+    }
+}
